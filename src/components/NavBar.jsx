c0da697d32ec94a44368f9b5c0951cf1f5cbcc75
Malak1805 <mjma200018@gmail.com>
2025-08-27 11:38:28 +0300

    added navbar links + in app.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router"
 
 
 const NavBar = () => {
 return(
   <>
-
+<Link to='/'>Home</Link>
+<Link to='/donations'>Donations</Link>
+<Link to='/auth/login'>Login</Link>
+<Link to='/auth/register'>Register</Link>
   </>
 )
 
